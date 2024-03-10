a23ff4ac215ff06ff62b053d1704439b7cc47def
Zach Kirlew <zachkirlew@gmail.com>
2024-03-10 08:56:02 +0000

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn healthcheck() -> impl Responder {
     HttpResponse::Ok()
 }
 
 pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health-check", web::get().to(healthcheck))
-    })
+    let server = HttpServer::new(|| App::new().route("/health-check", web::get().to(healthcheck)))
         .bind("127.0.0.1:8000")?
         .run();
     Ok(server)
