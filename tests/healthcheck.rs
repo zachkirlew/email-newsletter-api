dd25a3cffc524c661d1cb39ac7c8e18225738b0c
Zach Kirlew <zachkirlew@gmail.com>
2024-03-11 17:07:16 +0000

    Use random free port for app in IT

--- tests/healthcheck.rs
+++ tests/healthcheck.rs
@@ -1,15 +1,15 @@
-use actix_web::rt::spawn;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health-check")
+        .get(&format!("{}/health-check", address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -19,7 +19,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = email_newsletter_api::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Port 0 will trigger an OS scan for an available port which will then be bound to the application.
+    let port = listener.local_addr().unwrap().port();
+    let server = email_newsletter_api::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
