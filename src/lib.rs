f0a8f2d8d64530950bc1f4f54840c71f0bcfb5fa
Zach Kirlew <zachkirlew@gmail.com>
2024-04-03 06:35:32 +0100

    Add new subscriber validations from form data (#9)

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub mod configuration;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
+
+mod domain;
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,3 +1,4 @@
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 use actix_web::web::Form;
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
@@ -20,23 +21,41 @@ subscriber_name = % form.name
 )
 ]
 pub async fn subscribe(form: Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
-    match insert_subscriber(&form, &db_pool).await {
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    match insert_subscriber(&db_pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { name, email })
+    }
+}
+
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, db_pool)
+    skip(db_pool, new_subscriber)
 )]
-pub async fn insert_subscriber(form: &FormData, db_pool: &PgPool) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(
+    db_pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (name, email, subscribed_at) VALUES ($1, $2, $3)
         "#,
-        form.name,
-        form.email,
+        new_subscriber.name.as_ref(),
+        new_subscriber.email.as_ref(),
         Utc::now()
     )
     .execute(db_pool)
